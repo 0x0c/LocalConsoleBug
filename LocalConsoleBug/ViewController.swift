a28e333bf3e66b7f8d3aa648b71cc410b618b959
Akira <akira.matsuda@me.com>
2023-01-12 12:34:56 +0900

    Fix UI

--- LocalConsoleBug/ViewController.swift
+++ LocalConsoleBug/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Akira Matsuda on 2023/01/12.
 //
 
+import LocalConsole
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,6 +15,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func show(_ sender: Any) {
+        LCManager.shared.isVisible = true
+    }
+    
+    @IBAction func hide(_ sender: Any) {
+        LCManager.shared.isVisible = false
+    }
 }
 
